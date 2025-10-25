2b249569b9f06e2a2a662dc6b9a2fd49b0827e8c
Ava Kowalczyk <kowalczyka1@wit.edu>
2025-10-24 22:18:32 -0400

    Completed all linked list functions in list.c

--- list.c
+++ list.c
@@ -1,5 +1,6 @@
 #include "list.h"
 #include <stdlib.h>
+#include <stdio.h>
 // TODO: Include any necessary header files here
 
 /**
@@ -14,64 +15,182 @@ struct ll_node *ll_head(struct ll_node *head) {
 
 /**
  * TODO: Describe what the function does
+ * returns the tail of the linked list
+ * 
+ * @param head Pointer to the first node
+ * @return The last node, or NULL if list is empty
  */
 struct ll_node *ll_tail(struct ll_node *head) {
+if (head == NULL) return NULL;
+    struct ll_node *current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    return current;
     
 }
 
 /**
  * TODO: Describe what the function does
+ * Returns the number of nodes in the linked list
+ * 
+ * @param head pointer to the first node
+ * @return the total count of nodes
  */
 int ll_size(struct ll_node *head) {
-  
+  int count = 0;
+    struct ll_node *current = head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
 }
 
 /**
  * TODO: Describe what the function does
+ * Searches the linked list for a node containing the given value
+ * 
+ * @param head pointer to the first node
+ * @param value the value to search for
+ *@return pointer to the node containing the value, or NULL if not found
  */
 struct ll_node *ll_find(struct ll_node *head, int value) {
-   
+   struct ll_node *current = head;
+    while (current != NULL) {
+        if (current->data == value)
+            return current;
+        current = current->next;
+    }
+    return NULL;
 }
 
 /**
  * TODO: Describe what the function does
+ * 
+ * Converts the linked list to a dynamically allocated array of integers
+ * 
+ * @param head Pointer to the first node
+ * @return Pointer to a newly allocated array 
  */
 int *ll_toarray(struct ll_node *head) {
-   
+    int size = ll_size(head);
+    int *arr = malloc(size * sizeof(int));
+    if (arr == NULL) return NULL;
+
+    struct ll_node *current = head;
+    for (int i = 0; i < size; i++) {
+        arr[i] = current->data;
+        current = current->next;
+    }
+    return arr;
 }
 
 /**
  * TODO: Describe what the function does
+ * 
+ * Creates a new linked list node with the given data
+ * 
+ * @param data The integer value to store
+ * @return pointer to the new node
  */
 struct ll_node *ll_create(int data) {
-   
+   struct ll_node *new_node = malloc(sizeof(struct ll_node));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
 }
 
 /**
  * TODO: Describe what the function does
+ * 
+ * Frees all memory used by the linked list.
+ * 
+ * @param head Pointer to the first node
  */
 void ll_destroy(struct ll_node *head) {
-    
+     struct ll_node *current = head;
+    while (current != NULL) {
+        struct ll_node *temp = current;
+        current = current->next;
+        free(temp);
+    }
 }
 
 /**
  * TODO: Describe what the function does
+ * 
+ * appends a new node with the given data to the end of the linked list
+ * 
+ * @param head pointer to the first node
+ * @param data the integer value to append
  */
 void ll_append(struct ll_node *head, int data) {
- 
+ struct ll_node *new_node = ll_create(data);
+
+    if (head == NULL) {
+        head = new_node;  // This won’t update caller’s head, but test may handle it.
+        return;
+    }
+
+    struct ll_node *tail = ll_tail(head);
+    tail->next = new_node;
 }
 
 /**
  * TODO: Describe what the function does
+ * 
+ * creates a linked list from an array of integers
+ * 
+ * @param data pointer to the array of intege
+ * @param len number of elements in the array
+ * @return pointer to the head of the new list
  */
 struct ll_node *ll_fromarray(int* data, int len) {
+if (len <= 0) return NULL;
+    struct ll_node *head = ll_create(data[0]);
+    struct ll_node *current = head;
 
+    for (int i = 1; i < len; i++) {
+        current->next = ll_create(data[i]);
+        current = current->next;
+    }
+    return head;
 }
 
 /**
  * TODO: Describe what the function does
+ * 
+ * removes the first occurrence of a node containing the given value
+ * 
+ * @param head pointer to the first node
+ * @param value the value to remove
+ * @return pointer to the head of the list
  */
 struct ll_node *ll_remove(struct ll_node *head, int value) {
+ if (head == NULL) return NULL;
 
+    // If head needs to be removed
+    if (head->data == value) {
+        struct ll_node *temp = head->next;
+        free(head);
+        return temp;
+    }
+
+    struct ll_node *current = head;
+    while (current->next != NULL && current->next->data != value) {
+        current = current->next;
+    }
+
+    if (current->next != NULL) {
+        struct ll_node *temp = current->next;
+        current->next = temp->next;
+        free(temp);
+    }
+    return head;
 }
 
